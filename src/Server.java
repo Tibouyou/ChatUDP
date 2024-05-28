a17ed377f12a86514de13730ba43642c9c52a0bf
Tibouyou <tibouyou01@gmail.com>
2024-05-28 11:06:21 +0200

    log chat in txt file

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.net.*;
 import java.util.HashMap;
 
@@ -6,10 +8,16 @@ public class Server implements Runnable{
     HashMap<String, InetAddress> clientsAdress = new HashMap<>();
     HashMap<String, Integer> clientsPort = new HashMap<>();
 
+    String logFile;
+
     public Server() {
         try {
             server = new DatagramSocket(25565);
-        } catch (SocketException e) {
+            logFile = "logs/log.txt";
+            FileOutputStream fos = new FileOutputStream(logFile, true);
+            fos.write(("Server started\n").getBytes());
+            fos.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -24,7 +32,14 @@ public class Server implements Runnable{
                 String pseudo = message.split(":")[1];
                 clientsAdress.put(pseudo, packet.getAddress());
                 clientsPort.put(pseudo, packet.getPort());
-                System.out.println("Client " + pseudo + " connected");
+                System.out.println("Client " + pseudo + " connected\n");
+                try {
+                    FileOutputStream fos = new FileOutputStream(logFile, true);
+                    fos.write(("Client " + pseudo + " connected\n").getBytes());
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
                 String messageToSend = "Welcome on server Miguel (=^・ェ・^=) " + pseudo + " !";
                 DatagramPacket response = new DatagramPacket(messageToSend.getBytes(), messageToSend.length(), packet.getAddress(), packet.getPort());
                 server.send(response);
@@ -33,11 +48,13 @@ public class Server implements Runnable{
             String pseudoSender = clientsAdress.entrySet().stream().filter(entry -> entry.getValue().equals(packet.getAddress())).findFirst().get().getKey();
             message = pseudoSender + " : " + message;
             System.out.println(message);
+            FileOutputStream fos = new FileOutputStream(logFile, true);
+            fos.write((clientsAdress.get(pseudoSender)+":"+clientsPort.get(pseudoSender)+"/"+message+"\n").getBytes());
+            fos.close();
             for (String pseudo : clientsAdress.keySet()) {
                 DatagramPacket response = new DatagramPacket(message.getBytes(), message.length(), clientsAdress.get(pseudo), clientsPort.get(pseudo));
                 server.send(response);
             }
-            System.out.println(clientsAdress);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -49,6 +66,18 @@ public class Server implements Runnable{
 
     @Override
     public void run() {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                try {
+                    FileOutputStream fos = new FileOutputStream(logFile, true);
+                    fos.write(("Server stopped\n").getBytes());
+                    fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }));
+
         while (true) {
             listen();
         }
